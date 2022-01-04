2fe52b82e3ee1386b7ff7760c464fb0b5c62389e
David Henry <77511476+aG00Dtime@users.noreply.github.com>
2022-01-04 15:52:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from time import sleep as wait
-
+import os
 import keyboard
 import pyautogui
 
 # move mouse to the top left of the screen to trigger
 pyautogui.FAILSAFE = True
 
+# image folder path
+images_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images")
+
 # what items to buy
 buy_covenants = True
 buy_mystics = True
 
 # item buy button x & y offset
-
 # adjust this to the click the buy button to the right of the item
 button_add_x = 800  # higher value = further right
 button_add_y = 20  # higher value = further down
 
 # start delay
-start_timer = 2
+start_timer = 5
 
 # count var
 c = 0
@@ -29,25 +31,27 @@ scrolled = False
 bought_c = False
 bought_m = False
 
-print("Running...")
+print(f"Running in {start_timer}...")
 
+wait(start_timer)
 # hold down q to end
 while not keyboard.is_pressed('q') or not (buy_covenants and buy_mystics):
 
-    in_shop = pyautogui.locateCenterOnScreen("images/shop.png", confidence=.90)
+    in_shop = pyautogui.locateCenterOnScreen(os.path.join(images_folder, "shop.png"), confidence=.90)
 
     if in_shop:
         # delay
-        wait(start_timer)
+        
 
         # mystic bookmarks
-        mystic = pyautogui.locateCenterOnScreen("images/mystic_icon.png", confidence=.90)
+        mystic = pyautogui.locateCenterOnScreen(os.path.join(images_folder, "mystic_icon.png"), confidence=.90)
 
         # covenant bookmarks
-        covenant = pyautogui.locateCenterOnScreen("images/covenant_icon.png", confidence=.90)
+        covenant = pyautogui.locateCenterOnScreen(os.path.join(images_folder, "covenant_icon.png"), confidence=.90)
 
         # refresh button
-        refresh_button = pyautogui.locateCenterOnScreen("images/refresh_button.png", confidence=.90)
+        refresh_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder, "refresh_button.png"),
+                                                        confidence=.90)
 
         if mystic and not bought_m and buy_mystics:
             wait(0.5)
@@ -61,8 +65,10 @@ while not keyboard.is_pressed('q') or not (buy_covenants and buy_mystics):
 
             wait(0.5)
             # find and click the confirm button
-            mystic_confirm_buy_button = pyautogui.locateCenterOnScreen("images/mystic_buy_button.png",
+            mystic_confirm_buy_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder, "mystic_buy_button"
+                                                                                                   ".png"),
                                                                        confidence=.80)
+
             pyautogui.click(mystic_confirm_buy_button, button='left')
             wait(0.5)
 
@@ -82,8 +88,9 @@ while not keyboard.is_pressed('q') or not (buy_covenants and buy_mystics):
 
             wait(0.5)
             # find and click the confirm button
-            covenant_confirm_buy_button = pyautogui.locateCenterOnScreen("images/covenant_buy_button.png",
+            covenant_confirm_buy_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder,"covenant_buy_button.png"),
                                                                          confidence=.80)
+
             pyautogui.click(covenant_confirm_buy_button, button='left', clicks=2)
             wait(0.5)
 
@@ -118,7 +125,7 @@ while not keyboard.is_pressed('q') or not (buy_covenants and buy_mystics):
             wait(0.5)
 
             # find and click refresh confirm button
-            refresh_button_confirm = pyautogui.locateCenterOnScreen("images/confirm_refresh_button.png",
+            refresh_button_confirm = pyautogui.locateCenterOnScreen(os.path.join(images_folder,"confirm_refresh_button.png"),
                                                                     confidence=.90)
             pyautogui.click(refresh_button_confirm, button='left')
 
